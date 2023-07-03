9678c65581d7f375c540643b15cc6919e97428f2
Christine Lea <christine87@live.com.au>
2023-07-03 13:28:03 +1000

    "fetch data function added"

---
+++ script.js
@@ -0,0 +1,18 @@
+// test fetch & API
+let obj;
+
+function getWeatherApi() {
+	let requestUrl =
+		"https://api.openweathermap.org/data/2.5/forecast?lat=-37.81&lon=144.96&units=metric&appid=7efeea0385eeddc77479b9ad9143d71b";
+	fetch(requestUrl)
+		.then((response) => response.json())
+		.then((data) => {
+            // obj = data;
+            // console.log(obj);
+            getForecast(data);
+		});
+}
+
+function getForecast(data) {
+   
+}

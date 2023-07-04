7399bae73c966aa98491df859431fe8fdca911dc
Christine Lea <christine87@live.com.au>
2023-07-04 10:58:01 +1000

    "display forecast to HTML"

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // test fetch & API
 let obj;
 
+
 function getWeatherApi() {
 	let requestUrl =
 		"https://api.openweathermap.org/data/2.5/forecast?lat=-37.81&lon=144.96&units=metric&appid=7efeea0385eeddc77479b9ad9143d71b";
@@ -16,16 +17,19 @@ function getWeatherApi() {
 			let wind1 = data.list[8].wind.speed;
 			let humidity1 = data.list[8].main.humidity;
 
-			let dateEl = new Date(date1 * 1000).toLocaleString();
+			let dateFormat = new Date(date1 * 1000).toLocaleString();
+			let date = dayjs(dateFormat).format("DD MMM");
+
+			let dateEl = $("<h3>").addClass("card-title").text(date);
 			let iconEl = $("<img>").attr(
 				"src",
 				"https://openweathermap.org/img/wn/" + icon1 + "@2x.png"
-			);
-			let tempEl = $("<div>").text(`Temperature: ${temp1} °C`);
-			let windEl = $("<div>").text(`Wind Speed: ${wind1} MPH`);
-			let humidityEl = $("<div>").text(`Humidity: ${humidity1} %`);
+			).addClass("card-icon");
+			let tempEl = $("<p>").addClass("temp").text(`Temp: ${temp1} °C`);
+			let windEl = $("<p>").addClass("wind").text(`Wind: ${wind1} MPH`);
+			let humidityEl = $("<p>").addClass("humidity").text(`Humidity: ${humidity1} %`);
 
-			let cardsEl = $(".cards");
-			cardsEl.append(dateEl, iconEl, tempEl, windEl, humidityEl);
+			let cardEl = $(".card");
+			cardEl.append(dateEl, iconEl, tempEl, windEl, humidityEl);
 		});
 }

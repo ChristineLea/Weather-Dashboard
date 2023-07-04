94069c1ce71cede8c2c5cbff13f3b8699ecc4d82
Christine Lea <christine87@live.com.au>
2023-07-04 12:37:56 +1000

    "populate all weather data correctly"

--- script.js
+++ script.js
@@ -16,17 +16,19 @@ function getWeatherApi() {
 				let dataList = data.list[i];
 				populate(dataList);
 			}
+			let requestLocation = data.city.name;
+			console.log(requestLocation);
 			let todayData = data.list[0];
-			populateToday(todayData);
+			populateToday(requestLocation, todayData);
 		});
 }
 
-function populate(d) {
-	let dateData = d.dt;
-	let iconData = d.weather[0].icon;
-	let tempData = d.main.temp;
-	let windData = d.wind.speed;
-	let humidityData = d.main.humidity;
+function populate(list) {
+	let dateData = list.dt;
+	let iconData = list.weather[0].icon;
+	let tempData = list.main.temp;
+	let windData = list.wind.speed;
+	let humidityData = list.main.humidity;
 
 	let dateFormat = new Date(dateData * 1000).toLocaleString();
 	let date = dayjs(dateFormat).format("dddd, DD MMM");
@@ -50,7 +52,8 @@ function populate(d) {
 	cardsGrid.append(cardEl);
 }
 
-function populateToday(d) {
+function populateToday(id, d) {
+	let locationData = id;
 	let dateData = d.dt;
 	let iconData = d.weather[0].icon;
 	let tempData = d.main.temp;
@@ -60,18 +63,20 @@ function populateToday(d) {
 	let dateFormat = new Date(dateData * 1000).toLocaleString();
 	let date = dayjs(dateFormat).format("dddd, DD MMM");
 
-	let dateEl = $("<h2>").text(date);
-	let iconEl = $("<img>")
+	let locationEl = $("<h2>").addClass("location").text(locationData);
+	let dateEl = $("<h3>").addClass("date").text(date);
+	let iconEl = $("<img>").addClass("icon")
 		.attr(
 			"src",
 			"https://openweathermap.org/img/wn/" + iconData + "@2x.png"
 		)
 
-	let tempEl = $("<p>").text(`Temp: ${tempData} °C`);
-	let windEl = $("<p>").text(`Wind: ${windData} MPH`);
+	let tempEl = $("<p>").addClass("info").text(`Temp: ${tempData} °C`);
+	let windEl = $("<p>").addClass("info").text(`Wind: ${windData} MPH`);
 	let humidityEl = $("<p>")
+		.addClass("info")
 		.text(`Humidity: ${humidityData} %`);
 
-	let today = $(".today");
-	today.append(dateEl, iconEl, tempEl, windEl, humidityEl);
+	let todayEl = $(".today");
+	todayEl.append(locationEl, dateEl, iconEl, tempEl, windEl, humidityEl);
 }

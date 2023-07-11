5aac548e7558bfbd038ce9bb06444b7ceb04b533
Christine Lea <christine87@live.com.au>
2023-07-11 18:13:41 +1000

    "add javascript comments"

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
+// GLOBAL VARIABLES
 const searchBtn = $("#search");
 const API_KEY = "7efeea0385eeddc77479b9ad9143d71b";
 const $storageList = $("#storage-list");
 let storage = [];
 
-function renderStorageList() {
-	$storageList.html("");
-	for (let i = 0; i < storage.length; i++) {
-		let search = storage[i];
-		let locationEl = $(`<button>${search}</button>`);
-		locationEl.addClass("btn").attr("id", search);
-		// add attr of id of city name?
-		console.log(typeof search);
-		console.log(search);
-		$storageList.append(locationEl);
-	}
-	// append emptyNodes
-	appendTodayNodes();
-}
-
-// on click ".btn" check for "id"
-// pass id to getLatLon to render weather
-
-// get storage to weather
-
+// 2. Check local storage / Invoke renderStorageList()
 function init() {
 	let history = JSON.parse(localStorage.getItem("storage"));
 	if (history !== null) {
 		storage = history;
 	}
-
 	renderStorageList();
 }
 
+// 3. For every element in storage array, create/append a button for each stored location
+function renderStorageList() {
+	$storageList.html("");
+	for (let i = 0; i < storage.length; i++) {
+		let search = storage[i];
+		let locationEl = $(`<button>${search}</button>`);
+
+		locationEl.addClass("btn").attr("id", search);
+		$storageList.append(locationEl);
+	}
+	appendTodayNodes();
+}
+
+// 4. Create and append node elements used to display Today's weather
+// This stops node elements from been duplicated when additional locations are checked
+function appendTodayNodes() {
+	$(".today").append(
+		$("<h2>"),
+		$("<h3>").addClass("normal"),
+		$("<img>"),
+		$("<p>"),
+		$("<p>"),
+		$("<p>")
+	);
+	appendForecastNodes();
+}
+
+// 5. Create and append node elements used to display 5 day Forecast
+function appendForecastNodes() {
+	for (let i = 0; i <= 4; i++) {
+		$(".cards").append(
+			$("<div><h4></h4><img/><p></p><p></p><p></p></div>")
+		);
+	}
+
+	// Assign each element with a class to set their position in the HTML document
+	$(".cards").children().eq(0).addClass("card-zero");
+	$(".cards").children().eq(1).addClass("card-one");
+	$(".cards").children().eq(2).addClass("card-two");
+	$(".cards").children().eq(3).addClass("card-three");
+	$(".cards").children().eq(4).addClass("card-four");
+}
+
+// 7. Add search to local storage / render storage list
 function setLocalStorage() {
 	localStorage.setItem("storage", JSON.stringify(storage));
 	renderStorageList();
 }
 
-// Uses the geolocation API to convert the city, country to lat, lon
+// 7. Uses the geolocation API to convert the city, country to lat, lon format
 function getLatLon(city, country) {
-	// GET lat lon
 	let locationUrl =
 		"http://api.openweathermap.org/geo/1.0/direct?q=" +
 		city +
@@ -49,6 +73,7 @@ function getLatLon(city, country) {
 		API_KEY;
 
 	// Set integer to a two decimal format to meet requirements of weather API
+	// Invoke the getWeatherApi()
 	fetch(locationUrl)
 		.then(function (response) {
 			return response.json();
@@ -61,7 +86,7 @@ function getLatLon(city, country) {
 		});
 }
 
-// GET weather from lat & lon
+// 8. Use lat & lon to GET the weather/forecast from the API
 function getWeatherApi(lat, lon) {
 	let latitude = lat;
 	let longitude = lon;
@@ -78,24 +103,23 @@ function getWeatherApi(lat, lon) {
 			return response.json();
 		})
 		.then(function (data) {
-			// for (let i = 7; i <= 40; i += 8) {
-			// 	// cycle through get request to access the relevant data for 5 day forecast
-			// 	let dataList = data.list[i];
-			// 	populate(dataList);
-			// }
+			// send data to sortObj()
 			sortObj(data);
-			// Cycle data for location & todays weather
+			// Cycle data for Location
 			let requestLocation = data.city.name;
-			console.log(requestLocation);
+			// Cycle data for Today weather, which will be at index 0
 			let todayData = data.list[0];
+			// Pass location and today weather object to populateToday()
 			populateToday(requestLocation, todayData);
 		});
 }
 
-// Uses weather data to populate 5 day forecast
+// 9. Use weather data to populate 5 day forecast
 function sortObj(data) {
 	// for loop to pull data lists
-	// then loop data.list[j].dt
+	// GET request returns an object with 40 timestamps
+	// timestamp at index [7] will be day 1 forecast
+	// set the corresponding [index] to each day of the forecast & populate the data
 	let obj;
 	for (let j = 7; j <= 40; j += 8) {
 		console.log(j);
@@ -118,17 +142,12 @@ function sortObj(data) {
 			console.log("error");
 		}
 	}
-
-	// console.log(data.list[7]); //card-zero
-	// console.log(data.list[15]); //card-one
-	// console.log(data.list[23]); //card-two
-	// console.log(data.list[31]); //card-three
-	// console.log(data.list[39]); //card-four
-
-
-	// cycle through get request to access the relevant data for 5 day forecast
 }
+
+// 10. Populate the Forecast Weather
 function populate(obj, card) {
+	// Each time this function is called it will be passed the next forecast date data & the matching card's class
+	// Populate data by updating the .text() content of each node element
 	let $card = card;
 
 	// DATE
@@ -152,60 +171,20 @@ function populate(obj, card) {
 				"@2x.png"
 		);
 
-	$card
-		.children()
-		.eq(2)
-		.text(`Temp:  ${obj.main.temp}°C`);
-
-	$card
-		.children()
-		.eq(3)
-		.text(`Wind:  ${obj.wind.speed} mpH`);
-
-	$card
-		.children()
-		.eq(4)
-		.text(`Humidity: ${obj.main.humidity}%`);
+	// TEMP
+	$card.children().eq(2).text(`Temp:  ${obj.main.temp}°C`);
+	// WIND
+	$card.children().eq(3).text(`Wind:  ${obj.wind.speed} mpH`);
+	// HUMIDITY
+	$card.children().eq(4).text(`Humidity: ${obj.main.humidity}%`);
 }
 
-
-
-function appendForecastNodes() {
-	for (let i = 0; i <= 4; i++) {
-		$(".cards").append(
-			$("<div><h4></h4><img/><p></p><p></p><p></p></div>")
-		);
-	}
-
-	$(".cards").children().eq(0).addClass("card-zero");
-	$(".cards").children().eq(1).addClass("card-one");
-	$(".cards").children().eq(2).addClass("card-two");
-	$(".cards").children().eq(3).addClass("card-three");
-	$(".cards").children().eq(4).addClass("card-four");
-}
-
-function appendTodayNodes() {
-	// Append empty node elements for Today
-	$(".today").append(
-		$("<h2>"),
-		$("<h3>").addClass("normal"),
-		$("<img>"),
-		$("<p>"),
-		$("<p>"),
-		$("<p>")
-	);
-	appendForecastNodes();
-	// populateToday() will update text content to display
-	// INVOKE ON PAGE LOAD
-	// This stops node elements from been duplicated when additional locations are checked
-}
-
-// Uses weather data to populate Today's weather
+// 11. Uses weather data to populate Today's weather
 function populateToday(id, d) {
-	// Update content of appended node elements
-
 	// format the date to display
 	let dateFormat = new Date(d.dt * 1000).toLocaleString();
+
+	// Populate data by updating the .text() content of each node element
 	// <h2> node to populate/display location data
 	$(".today").children().eq(0).text(id);
 
@@ -229,14 +208,18 @@ function populateToday(id, d) {
 	$(".today").children().eq(5).text(`Humidity: ${d.main.humidity}%`);
 }
 
-// CLEAR weather data shown
+// 6. CLICK EVENT triggered when a user searches a location
 $(".form").on("click", ".form-btn", function (e) {
 	e.preventDefault();
 
-	// DOM traversal to access the value from city / country input
+	// DOM traversal to access the value from city input / country input
 	let city = $(this).parent().children().eq(2).val();
 	let country = $(this).prev().val();
+	// Invoke the function using the input value
 	getLatLon(city, country);
+
+	// Save location search in required format
+	// Check if the location is already existing in local storage then push to storage array
 	let addHistory = `${city}, ${country}`;
 
 	if (storage.includes(addHistory)) {
@@ -246,10 +229,10 @@ $(".form").on("click", ".form-btn", function (e) {
 		storage.push(addHistory);
 		$(":input", ".form").val("");
 	}
-
 	setLocalStorage();
 });
 
+// 1. On page load, this function will execute
 init();
 // TEST THAT LOCAL STORAGE WHEN CLICKED WILL OPEN WEATHER FOR THAT LOCATION
 $(".btn").on("click", function () {
